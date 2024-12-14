de0381d46ca7c60dea3e66bbce9ac200b17623a7
Konyee11 <konyee.527@gmail.com>
2024-12-14 21:41:17 +0900

    Expressサーバーを設定し、ユーザー用のルートを追加

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Server-side code running");
+import express from "express";
+const app = express();
+import usersRouter from "./routes/users.js";
+const PORT = 3000;
+
+// ミドルウェアの設定
+app.use("/api/users", usersRouter);
+
+app.listen(PORT, () =>
+    console.log(`Server is running on http://localhost:${PORT}`)
+);
