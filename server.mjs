9eafbc4ac02147b08fa64058b810424150a0c79f
Konyee11 <konyee.527@gmail.com>
2024-12-15 08:21:49 +0900

    dotenvを追加し、MongoDBへの接続を設定

--- server.mjs
+++ server.mjs
@@ -4,6 +4,15 @@ import userRouter from "./routes/users.mjs";
 import authRouter from "./routes/auth.mjs";
 import postRouter from "./routes/posts.mjs";
 const PORT = 3000;
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// データベースの接続
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("DBと接続中・・・"))
+    .catch((err) => console.log(err));
 
 // ミドルウェアの設定
 app.use("/api/users", userRouter);
@@ -11,5 +20,5 @@ app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 
 app.listen(PORT, () =>
-    console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`サーバーが起動しました：http://localhost:${PORT}`)
 );
