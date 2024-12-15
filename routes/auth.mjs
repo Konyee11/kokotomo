154e8c8daa8f6293109f10e796b74718edb4d080
Konyee11 <konyee.527@gmail.com>
2024-12-15 09:54:43 +0900

    bcryptを追加し、ユーザー登録時のパスワードをハッシュ化、ログイン時にパスワードを検証する機能を実装

--- routes/auth.mjs
+++ routes/auth.mjs
@@ -1,15 +1,22 @@
 import { Router } from "express";
 const router = Router();
+import bcrypt from "bcryptjs";
 import User from "../models/user.mjs";
 
 // ユーザーの登録
 router.post("/register", async (req, res) => {
     try {
+        // パスワードのハッシュ化
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+
+        // ユーザーの作成
         const newUser = await new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
         });
+
+        // ユーザーの保存
         await newUser.save();
         res.status(200).json(newUser);
     } catch (error) {
@@ -20,11 +27,27 @@ router.post("/register", async (req, res) => {
 // ユーザーのログイン
 router.post("/login", async (req, res) => {
     try {
+        // ユーザーの検索
         const user = await User.findOne({ email: req.body.email });
-        !user && res.status(404).json("ユーザーが見つかりません");
-        const validPassword = user.password === req.body.password;
-        !validPassword && res.status(400).json("パスワードが間違っています");
-        return res.status(200).json(user);
+        if (!user) {
+            return res
+                .status(400)
+                .json({ message: "ユーザーが見つかりません" });
+        }
+
+        // パスワードの検証
+        const validPassword = bcrypt.compareSync(
+            req.body.password,
+            user.password
+        );
+        if (!validPassword) {
+            return res
+                .status(400)
+                .json({ message: "パスワードが間違っています" });
+        }
+
+        // 認証成功
+        return res.status(200).json({ message: "ログイン成功", user });
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
