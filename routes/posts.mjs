8cf8abada9d3551341b1395193b43512c47d0048
Konyee11 <konyee.527@gmail.com>
2024-12-15 13:22:18 +0900

    投稿の作成機能を追加

--- routes/posts.mjs
+++ routes/posts.mjs
@@ -1,8 +1,16 @@
 import { Router } from "express";
 const router = Router();
+import Post from "../models/post.mjs";
 
-router.get("/", (req, res) => {
-    res.send("posts router");
+// 投稿の作成
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 export default router;
