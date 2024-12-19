d517de6b450bc9ccd0260804b41bfdf2716dd460
Konyee11 <konyee.527@gmail.com>
2024-12-19 21:51:36 +0900

    Rightbarコンポーネントにオンライン友達リストを表示する機能を追加し、スタイルを調整

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -1,4 +1,6 @@
 import "./Rightbar.scss";
+import Online from "../online/Online";
+import { Users } from "../../dummyData";
 
 export default function Rightbar() {
     return (
@@ -18,30 +20,10 @@ export default function Rightbar() {
                 <img src="/assets/event.jpeg" alt="" className="event__img" />
                 <h4 className="rightbar__title">オンラインの友達</h4>
                 <ul className="rightbar__friendlist">
-                    <li className="rightbar__friend">
-                        <div className="rightbar__friend__container">
-                            <img
-                                src="./assets/person/1.png"
-                                alt=""
-                                className="rightbar__friend__img"
-                            />
-                            <span className="rightbar__friend__online"></span>
-                        </div>
-                        <span className="rightbar__friend__username">
-                            Konyee
-                        </span>
-                    </li>
-                    <li className="rightbar__friend">
-                        <div className="rightbar__friend__container">
-                            <img
-                                src="./assets/person/2.jpeg"
-                                alt=""
-                                className="rightbar__friend__img"
-                            />
-                            <span className="rightbar__friend__online"></span>
-                        </div>
-                        <span className="rightbar__friend__username">John</span>
-                    </li>
+                    {Users.map((user) => (
+                        <Online key={user.id} user={user} />
+                    ))}
+                    ;
                 </ul>
                 <p className="promotion">プロモーション広告</p>
                 <img
