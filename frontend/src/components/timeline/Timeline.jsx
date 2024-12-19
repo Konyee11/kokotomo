47ab490ed7e379dad1ab5fb4954e568e1eee19e7
Konyee11 <konyee.527@gmail.com>
2024-12-19 19:46:51 +0900

    ユーザー情報と投稿データをダミーデータとして追加し、投稿コンポーネントを動的に更新

--- frontend/src/components/timeline/Timeline.jsx
+++ frontend/src/components/timeline/Timeline.jsx
@@ -1,16 +1,16 @@
 import "./Timeline.scss";
 import Share from "../share/Share";
 import Post from "../post/Post";
+import { Posts } from "../../dummyData";
 
 export default function Timeline() {
     return (
         <div className="timeline">
             <div className="timeline__wrapper">
                 <Share />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                {Posts.map((post) => (
+                    <Post key={post.id} post={post} />
+                ))}
             </div>
         </div>
     );
