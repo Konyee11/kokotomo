0e76c0badb25092802915636a0c4b6b5132ac9c8
Konyee11 <konyee.527@gmail.com>
2024-12-19 13:02:15 +0900

    投稿コンポーネントを新規作成し、タイムラインに追加

--- frontend/src/components/timeline/Timeline.jsx
+++ frontend/src/components/timeline/Timeline.jsx
@@ -1,12 +1,13 @@
 import "./Timeline.scss";
 import Share from "../share/Share";
+import Post from "../post/Post";
 
 export default function Timeline() {
     return (
         <div className="timeline">
             <div className="timeline__wrapper">
                 <Share />
-                {/* <Post /> */}
+                <Post />
             </div>
         </div>
     );
