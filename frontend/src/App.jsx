91452e3ce5cf8321071dbad3fe39e416d12792db
Konyee11 <konyee.527@gmail.com>
2024-12-20 19:01:25 +0900

    新規登録ページのコンポーネントとスタイルを追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 // import Home from "./pages/home/Home";
 // import Profile from "./pages/profile/Profile";
-import Login from "./pages/login/Login";
+// import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
     // return <Home />;
     // return <Profile />;
-    return <Login />;
+    // return <Login />;
+    return <Register />;
 }
 
 export default App;
