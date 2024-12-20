3d599e55d18dcdfa06d59d4da3365be29d058890
Konyee11 <konyee.527@gmail.com>
2024-12-20 20:16:04 +0900

    サイドバーにリンクを追加して、ホームとプロフィールへのナビゲーションを実装

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -10,6 +10,7 @@ import {
 import "./Sidebar.scss";
 import Friend from "../friend/Friend";
 import { Users } from "../../dummyData";
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
     console.log(Users);
@@ -20,7 +21,9 @@ export default function Sidebar() {
                 <ul className="sidebar__list">
                     <li className="sidebar__item">
                         <Home className="sidebar__icon" />
-                        <span className="sidebar__text">ホーム</span>
+                        <Link to="/">
+                            <span className="sidebar__text">ホーム</span>
+                        </Link>
                     </li>
                     <li className="sidebar__item">
                         <Search className="sidebar__icon" />
@@ -40,7 +43,9 @@ export default function Sidebar() {
                     </li>
                     <li className="sidebar__item">
                         <Person className="sidebar__icon" />
-                        <span className="sidebar__text">プロフィール</span>
+                        <Link to="/profile/username">
+                            <span className="sidebar__text">プロフィール</span>
+                        </Link>
                     </li>
                     <li className="sidebar__item">
                         <Settings className="sidebar__icon" />
