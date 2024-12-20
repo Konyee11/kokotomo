be6154b20a7ff2f4c952765b7e3740c963e1f50f
Konyee11 <konyee.527@gmail.com>
2024-12-21 07:57:52 +0900

    タイムラインコンポーネントでAPIから投稿を取得するように変更

--- client/src/components/timeline/Timeline.jsx
+++ client/src/components/timeline/Timeline.jsx
@@ -1,15 +1,31 @@
 import "./Timeline.scss";
 import Share from "../share/Share";
 import Post from "../post/Post";
-import { Posts } from "../../dummyData";
+import { useEffect, useState } from "react";
+// import { Posts } from "../../dummyData";
+import axios from "axios";
 
 export default function Timeline() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await axios.get(
+                "api/posts/timeline/675e4db9739c91b694fe66d6"
+            );
+            // console.log(response.data);
+
+            setPosts(response.data);
+        };
+        fetchPosts();
+    }, []);
+
     return (
         <div className="timeline">
             <div className="timeline__wrapper">
                 <Share />
-                {Posts.map((post) => (
-                    <Post key={post.id} post={post} />
+                {posts.map((post) => (
+                    <Post key={post._id} post={post} />
                 ))}
             </div>
         </div>
