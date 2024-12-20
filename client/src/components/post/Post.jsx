60ad9039bda7e23f111d10980d0d35c65ecc3d8c
Konyee11 <konyee.527@gmail.com>
2024-12-21 07:57:26 +0900

    投稿コンポーネントでユーザー情報をAPIから取得するように変更

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,16 +1,26 @@
 import "./Post.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MoreVert } from "@mui/icons-material";
-import { Users } from "../../dummyData";
+// import { Users } from "../../dummyData";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 export default function Post({ post }) {
     const PUBLIC_FOLDER = import.meta.env.VITE_PUBLIC_FOLDER;
 
-    const user = Users.filter((user) => user.id === post.userId);
-
     const [like, setLike] = useState(post.like);
     const [isLiked, setIsLiked] = useState(false);
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const response = await axios.get(`api/users/${post.userId}`);
+            console.log(response.data);
+
+            setUser(response.data);
+        };
+        fetchUser();
+    }, []);
 
     const handleLike = () => {
         setLike(isLiked ? like - 1 : like + 1);
@@ -23,13 +33,14 @@ export default function Post({ post }) {
                 <div className="post__top">
                     <div className="post__top__left">
                         <img
-                            src={PUBLIC_FOLDER + user[0].profilePicture}
+                            src={
+                                user.profilePicture ||
+                                PUBLIC_FOLDER + "/person/noAvatar.png"
+                            }
                             alt=""
                             className="post__profileimg"
                         />
-                        <span className="post__username">
-                            {user[0].username}
-                        </span>
+                        <span className="post__username">{user.username}</span>
                         <span className="post__date">{post.date}</span>
                     </div>
                     <div className="post__top__right">
