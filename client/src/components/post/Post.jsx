718d5a3aed13765bc7cac35b9ead07be89cfa5c8
Konyee11 <konyee.527@gmail.com>
2024-12-21 14:11:27 +0900

    投稿コンポーネントで日本語のタイムスタンプを表示し、ユーザー情報の取得方法を修正

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -4,44 +4,72 @@ import { MoreVert } from "@mui/icons-material";
 // import { Users } from "../../dummyData";
 import PropTypes from "prop-types";
 import axios from "axios";
+import { format, register } from "timeago.js";
+import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
     const PUBLIC_FOLDER = import.meta.env.VITE_PUBLIC_FOLDER;
 
-    const [like, setLike] = useState(post.like);
+    const [like, setLike] = useState(post.likes.length);
     const [isLiked, setIsLiked] = useState(false);
     const [user, setUser] = useState({});
 
     useEffect(() => {
         const fetchUser = async () => {
-            const response = await axios.get(`api/users/${post.userId}`);
-            console.log(response.data);
+            const response = await axios.get(`api/users?userId=${post.userId}`);
 
             setUser(response.data);
         };
         fetchUser();
-    }, []);
+    }, [post.userId]);
 
     const handleLike = () => {
         setLike(isLiked ? like - 1 : like + 1);
         setIsLiked(!isLiked);
     };
 
+    // 日本語ロケールの定義
+    const jaLocale = (number, index) =>
+        [
+            ["たった今", "すぐに"],
+            ["%s秒前", "%s秒以内"],
+            ["1分前", "1分以内"],
+            ["%s分前", "%s分以内"],
+            ["1時間前", "1時間以内"],
+            ["%s時間前", "%s時間以内"],
+            ["1日前", "1日以内"],
+            ["%s日前", "%s日以内"],
+            ["1週間前", "1週間以内"],
+            ["%s週間前", "%s週間以内"],
+            ["1ヶ月前", "1ヶ月以内"],
+            ["%sヶ月前", "%sヶ月以内"],
+            ["1年前", "1年以内"],
+            ["%s年前", "%s年以内"],
+        ][index];
+
+    // 日本語ロケールを登録
+    register("ja", jaLocale);
+
     return (
         <div className="post">
             <div className="post__wrapper">
                 <div className="post__top">
                     <div className="post__top__left">
-                        <img
-                            src={
-                                user.profilePicture ||
-                                PUBLIC_FOLDER + "/person/noAvatar.png"
-                            }
-                            alt=""
-                            className="post__profileimg"
-                        />
+                        <Link to={`/profile/${user.username}`}>
+                            <img
+                                src={
+                                    PUBLIC_FOLDER +
+                                    (user.profilePicture ||
+                                        "/person/noAvatar.png")
+                                }
+                                alt=""
+                                className="post__profileimg"
+                            />
+                        </Link>
                         <span className="post__username">{user.username}</span>
-                        <span className="post__date">{post.date}</span>
+                        <span className="post__date">
+                            {format(post.createdAt, "ja")}
+                        </span>
                     </div>
                     <div className="post__top__right">
                         <MoreVert />
@@ -50,7 +78,7 @@ export default function Post({ post }) {
                 <div className="post__center">
                     <span className="post__text">{post.desc}</span>
                     <img
-                        src={PUBLIC_FOLDER + post.photo}
+                        src={PUBLIC_FOLDER + post.img}
                         alt=""
                         className="post__img"
                     />
@@ -81,12 +109,12 @@ export default function Post({ post }) {
 // PropTypesを使用して型を定義
 Post.propTypes = {
     post: PropTypes.shape({
-        id: PropTypes.number.isRequired,
+        _id: PropTypes.string.isRequired,
         desc: PropTypes.string,
-        photo: PropTypes.string,
-        date: PropTypes.string,
-        userId: PropTypes.number.isRequired,
-        like: PropTypes.number,
+        img: PropTypes.string,
+        createdAt: PropTypes.string,
+        userId: PropTypes.string.isRequired,
+        likes: PropTypes.array,
         comment: PropTypes.number,
     }).isRequired,
 };
