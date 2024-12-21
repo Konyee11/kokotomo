7efa1c8c95e088c1a381914ab44d585c7d42a118
Konyee11 <konyee.527@gmail.com>
2024-12-21 14:10:57 +0900

    /profile/apiエンドポイントを/apiにリダイレクトする設定を追加

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,6 +9,10 @@ export default defineConfig({
             "/api": {
                 target: "http://localhost:3000",
             },
+            "/profile/api": {
+                target: "http://localhost:3000",
+                rewrite: (path) => path.replace(/^\/profile\/api/, "/api"), // `/profile/api` を `/api` に置き換え
+            },
         },
     },
 });
