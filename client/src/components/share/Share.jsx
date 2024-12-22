a61e8f3398d5196e298b9890649a6e8f7f9c5bc0
Konyee11 <konyee.527@gmail.com>
2024-12-22 21:12:10 +0900

    投稿機能を追加し、フォーム送信時にAPIを呼び出すように修正

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,12 +1,35 @@
-import { useContext } from "react";
 import "./Share.scss";
+import { useContext, useRef } from "react";
 import { Analytics, Face, GifBox, Image } from "@mui/icons-material";
 import { AuthContext } from "../../state/AuthContext";
+import axios from "axios";
 
 export default function Share() {
     const PUBLIC_FOLDER = import.meta.env.VITE_PUBLIC_FOLDER;
 
     const { user } = useContext(AuthContext);
+
+    const desc = useRef();
+
+    // 投稿ボタンをクリックしたときの処理
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // ページ遷移を防ぐ
+
+        // 新しい投稿を作成
+        const newPost = {
+            userId: user._id,
+            desc: desc.current.value,
+        };
+        console.log(newPost);
+
+        try {
+            // 投稿のAPIをたたく
+            await axios.post("api/posts/", newPost);
+            window.location.reload(); // ページをリロード
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <div className="share">
@@ -25,10 +48,15 @@ export default function Share() {
                         type="text"
                         placeholder="今の気持ちを投稿してみよう！"
                         className="share__input"
+                        ref={desc}
+                        id="desc"
                     />
                 </div>
                 <hr className="share__hr" />
-                <div className="share__buttons">
+                <form
+                    className="share__buttons"
+                    onSubmit={(e) => handleSubmit(e)}
+                >
                     <div className="share__options">
                         <div className="share__option">
                             <Image
@@ -59,8 +87,10 @@ export default function Share() {
                             <span className="share__option__text">投票</span>
                         </div>
                     </div>
-                    <button className="share__button">投稿</button>
-                </div>
+                    <button className="share__button" type="submit">
+                        投稿
+                    </button>
+                </form>
             </div>
         </div>
     );
