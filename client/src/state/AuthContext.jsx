c921457f8c762ac14a096c3d7eba939a8e45a171
Konyee11 <konyee.527@gmail.com>
2024-12-22 20:03:30 +0900

    AuthContext.jsxで初期ユーザー状態を定義し、ダミーデータを追加

--- client/src/state/AuthContext.jsx
+++ client/src/state/AuthContext.jsx
@@ -4,8 +4,28 @@ import AuthReducer from "./AuthReducer";
 
 // 最初のユーザー状態を定義
 const initialState = {
-    user: null,
+    // user: null,
+    user: {
+        _id: "675e5219685e82187795f011",
+        username: "Tanaka",
+
+        email: "tanaka@gmail.com",
+
+        password:
+            "$2a$10$eXhOS4m5yMM32bW5c9b4D.skwhdJe/buDq9gQPqi2IB3dUW5VixiC",
+
+        profilePicture: "/person/2.jpg",
+
+        coverPicture: "",
+
+        followers: [],
+
+        followings: [],
+
+        isAdmin: false,
+    },
     isFetching: false,
+    error: false,
 };
 
 // 状態をグローバルに管理するためのコンテキストを作成
