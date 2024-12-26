e4a85328a7f4647b1b7aed9d6097a9b3453353de
Konyee11 <konyee.527@gmail.com>
2024-12-26 20:57:22 +0900

    タイムラインコンポーネントにタブ切り替え機能を追加し、最近の投稿を取得するAPIエンドポイントを実装

--- client/src/components/timeline/Timeline.jsx
+++ client/src/components/timeline/Timeline.jsx
@@ -1,6 +1,7 @@
 import "./Timeline.scss";
 import Share from "../share/Share";
 import Post from "../post/Post";
+import Switchbar from "../switchbar/Switchbar";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
@@ -8,28 +9,49 @@ import { AuthContext } from "../../state/AuthContext";
 
 export default function Timeline({ username }) {
     const [posts, setPosts] = useState([]);
+    const [activeTab, setActiveTab] = useState("timeline");
 
     const { user } = useContext(AuthContext);
 
     useEffect(() => {
         const fetchPosts = async () => {
-            const response = username
-                ? await axios.get(`api/posts/users/${username}`) // プロフィールの場合
-                : await axios.get(`api/posts/timeline/${user._id}`); // ホームの場合
+            let endpoint;
+            if (username) {
+                // ユーザーネームがある＝プロフィールページの場合
+                endpoint = `api/posts/users/${username}`;
+            } else if (activeTab === "timeline") {
+                // ホームの場合
+                endpoint = `api/posts/timeline/${user._id}`;
+            } else if (activeTab === "recent") {
+                // おすすめの場合
+                endpoint = "api/recent-posts";
+            }
 
-            setPosts(
-                response.data.sort((p1, p2) => {
-                    return new Date(p2.createdAt) - new Date(p1.createdAt);
-                })
-            );
+            try {
+                const response = await axios.get(endpoint);
+                setPosts(
+                    response.data.sort((p1, p2) => {
+                        return new Date(p2.createdAt) - new Date(p1.createdAt);
+                    })
+                );
+            } catch (error) {
+                console.log(error);
+            }
         };
         fetchPosts();
-    }, [username, user]);
+    }, [username, user, activeTab]);
 
     return (
         <div className="timeline">
             <div className="timeline__wrapper">
                 <Share />
+                {/* プロフィールページではSwitchbarを表示させない */}
+                {!username && (
+                    <Switchbar
+                        activeTab={activeTab}
+                        setActiveTab={setActiveTab}
+                    />
+                )}
                 {posts.map((post) => (
                     <Post key={post._id} post={post} />
                 ))}
