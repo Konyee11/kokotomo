7c80e67457bed21d8b5dde92f5a8f5e59003bbdc
Konyee11 <konyee.527@gmail.com>
2025-01-09 22:55:47 +0900

    トップバーコンポーネントのリファクタリングを実施し、ユーザーのプロフィール画像取得処理を改善。アイコンとバッジをまとめた小さなUIコンポーネントを追加し、コードの可読性を向上。

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,21 +1,32 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { Search, Chat, Notifications } from "@mui/icons-material";
 import "./Topbar.scss";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
 import { AuthContext } from "../../state/AuthContext";
+import PropTypes from "prop-types";
 
+const APP_TITLE = "Kokotomo";
+const NO_AVATAR_IMAGE = "/images/noAvatar.png";
+
+/**
+ * トップバーコンポーネント
+ */
 export default function Topbar() {
-    const PUBLIC_FOLDER = import.meta.env.VITE_PUBLIC_FOLDER;
-
     const { user } = useContext(AuthContext);
+
+    // ユーザーのプロフィール画像を取得
+    const userAvatar = user?.profilePicture || NO_AVATAR_IMAGE;
 
     return (
         <div className="topbar">
+            {/* Left Section */}
             <div className="topbar__left">
                 <Link to="/">
-                    <span className="topbar__logo">Kokotomo</span>
+                    <span className="topbar__logo">{APP_TITLE}</span>
                 </Link>
             </div>
+
+            {/* Center Section */}
             <div className="topbar__center">
                 <div className="searchbar">
                     <Search className="searchbar__icon" htmlColor="black" />
@@ -26,24 +37,16 @@ export default function Topbar() {
                     />
                 </div>
             </div>
+
+            {/* Right Section */}
             <div className="topbar__right">
                 <div className="topbar__iconItems">
-                    <div className="topbar__iconItem">
-                        <Chat />
-                        <span className="topbar__iconBadge">1</span>
-                    </div>
-                    <div className="topbar__iconItem">
-                        <Notifications />
-                        <span className="topbar__iconBadge">2</span>
-                    </div>
-                    <Link to={`/profile/${user.username}`}>
+                    <IconItem icon={<Chat />} badge="2" />
+                    <IconItem icon={<Notifications />} badge="3" />
+                    <Link to={`/profile/${user?.username}`}>
                         <img
-                            src={
-                                user.profilePicture
-                                    ? PUBLIC_FOLDER + user.profilePicture
-                                    : PUBLIC_FOLDER + "/person/noAvatar.png"
-                            }
-                            alt="User"
+                            src={userAvatar}
+                            alt="プロフィール画像"
                             className="topbar__profileImg"
                         />
                     </Link>
@@ -52,3 +55,22 @@ export default function Topbar() {
         </div>
     );
 }
+
+/**
+ * アイコンとバッジをまとめた小さなUIコンポーネント
+ * @param {JSX.Element} icon アイコン要素 (例: <Chat />)
+ * @param {string | number} badge バッジに表示する数値
+ */
+function IconItem({ icon, badge }) {
+    return (
+        <div className="topbar__iconItem">
+            {icon}
+            <span className="topbar__iconBadge">{badge}</span>
+        </div>
+    );
+}
+
+IconItem.propTypes = {
+    icon: PropTypes.element.isRequired,
+    badge: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+};
